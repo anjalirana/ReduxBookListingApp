345063adf473371bd6cff57233fab963d137e24f
Anjali <anjali@Temandos-MacBook-Pro.local>
2017-03-23 13:57:02 +1000

    Simple book listing app built using React and Redux. React for front end and Redux for holding data and providing data to front end components

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'ABC', pages: 101},
+        {title: 'DEF', pages: 123},
+        {title: 'GHI', pages: 343},
+        {title: 'JKL', pages: 443}
+    ];
+}
